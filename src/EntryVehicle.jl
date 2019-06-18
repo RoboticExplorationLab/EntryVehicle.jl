30de2120bd98eb0d7b9b2cf85e12872ae174bf78
Zac <zacmanchester@stanford.edu>
2019-06-18 01:28:36 -0700

    Flat earth sim working

--- src/EntryVehicle.jl
+++ src/EntryVehicle.jl
@@ -2,7 +2,7 @@ module EntryVehicle
 
 using LinearAlgebra
 
-function vehicle_dynamics!(ẋ, x, u)
+function vehicle_dynamics!(ẋ, x, u, t)
 
     #Flat Earth
     Re = 6371.0 #Earth radius (km)
@@ -14,7 +14,8 @@ function vehicle_dynamics!(ẋ, x, u)
     r_cone = 1.3 #Cone radius (m)
     θ_cone = 70*pi/180 #Cone half-angle
     h_cone = r_cone/tan(θ_cone) #Cone height (m)
-    l_cone = 0.2 #Distance to cone COM from point (m)
+    A_cone = pi*r_cone*sqrt(r_cone^2 + h_cone^2) #(m^2)
+    com_cone = [0; 0; 0.2] #Vector to cone COM from point (m)
     Cp = 1.0 #pitch damping coefficient
 
     #Inertia in kg*m^2
@@ -29,24 +30,25 @@ function vehicle_dynamics!(ẋ, x, u)
 
     r = x[1:3] #position vector in ECI frame (km)
     q = x[4:7]/norm(x[4:7]) #body to ECI rotation
-    v = x[8:10] #ECI velocity vector (rad/s)
-    ω = x[11:13] #body frame angular velocity vector (km/s)
+    v = x[8:10] #ECI velocity vector (km/s)
+    ω = x[11:13] #body frame angular velocity vector (rad/s)
 
     #Calculate aerodynamic forces + torques in body frame
-    #h = norm(r) - Re #height above sea level (km)
-    #ρ = exponential_atmosphere(h) #atmospheric density (kg/m^3)
-    ρ = 1e-5
+    h = norm(r) - Re #height above sea level (km)
+    ρ = exponential_atmosphere(h) #atmospheric density (kg/m^3)
     vb = qrot(qconj(q),v);
     Fb = [0; 0; 0]
-    tau = [0; 0; 0]
+    τ = [0; 0; 0]
+    A = 0;
     dr = 0.1;
     dθ = pi/10;
     for ri in dr:dr:r_cone
         for θ = 0:dθ:(2*pi-dθ)
             nb = [cos(θ); sin(θ); -h_cone/r_cone];
             nb = nb/norm(nb);
-            rc = ri*[cos(θ); sin(θ); h_cone/r_cone] - r_com;
+            rc = ri*[cos(θ); sin(θ); h_cone/r_cone] - com_cone;
             dA = ri*dθ*sqrt(dr^2 + (dr*h_cone/r_cone)^2);
+            A = A + dA
             if nb'*vb > 0
                 Fi = -0.5*ρ*dA*norm(vb)*nb*(nb'*vb);
                 Fb = Fb + Fi;
@@ -54,10 +56,10 @@ function vehicle_dynamics!(ẋ, x, u)
             end
         end
     end
-    Feci = qrot(q,Fb) - [0; 0; g]
+    Feci = qrot(q,(A_cone/A)*Fb) - [0; 0; g]
 
     #Add pitch damping
-    τ = τ - Cp*[ω[1:2]; 0]
+    τ = (A_cone/A)*τ - Cp*[ω[1:2]; 0]
 
     ẋ[1:3] = v
     ẋ[4:7] = 0.5*qmult(q,[0; ω])
@@ -78,8 +80,12 @@ function spherical_gravity(r)
 end
 
 function qmult(q1,q2)
-    q3 = [q1[1]*q2[1] - q1[2:4]'*q2[2:4]';
-          q1[1]*q2[2:4] + q2[1]*q1[2:4] + cross(q1[2:4], q2[2:4])]
+    s1 = q1[1]
+    s2 = q2[1]
+    v1 = q1[2:4]
+    v2 = q2[2:4]
+    q3 = [s1*s2 - v1'*v2;
+          s1*v2 + s2*v1 + cross(v1, v2)]
 end
 
 function qrot(q,x)
